6f42a33f268df49631ea2703060e00ebbe0f85bb
Nataniel Fiuza <natan.fiuza@gmail.com>
2025-09-16 22:33:18 -0300

    Teste de categorias

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,12 +1,14 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\CategoryDefault;
+use App\Models\CategoryUser;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -20,7 +22,7 @@ class ProfileController extends Controller
     {
         return Inertia::render('Profile/Edit', [
             'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
-            'status' => session('status'),
+            'status'          => session('status'),
         ]);
     }
 
@@ -34,6 +36,26 @@ class ProfileController extends Controller
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
+        
+        $categorias_padroes = CategoryDefault::all();
+        foreach ($categorias_padroes as $value) {
+            $category_user                      = new CategoryUser();
+            $category_user->user_id             = 1;
+            $category_user->category_default_id = $value->id;
+            $category_user->name                = $value->name;
+            $category_user->type                = $value->type;
+            $category_user->icon                = $value->icon;
+            $category_user->color               = $value->color;
+            $category_user->save();
+        }
+
+        $categorias = CategoryUser::where('user_id', 1)->get();
+
+        File::put(storage_path('logs/categorias.json'), json_encode(
+            [
+                'status' => 'success',
+                'data'   => $categorias,
+            ]));
 
         $request->user()->save();
 
